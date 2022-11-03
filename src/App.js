5a40e4c4807b227d58abb008ab702c3b245e3089
GasparKritsch <gaspar.kritsch.dev@gmail.com>
2022-11-03 15:05:43 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CarritoPage from './components/CarritoPage';
 function App() {
   return (
     <CartContextProvider>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<ItemListContainer />}/>
